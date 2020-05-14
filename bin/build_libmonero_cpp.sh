2978410bb0b7b1848c3293d2e2b13e57a6a574fc
woodser <woodser@protonmail.com>
2020-05-14 16:38:20 -0400

    build core and library with -j$HOST_NCORES

--- bin/build_libmonero_cpp.sh
+++ bin/build_libmonero_cpp.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+HOST_NCORES=$(nproc 2>/dev/null || shell nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)
+
 # Make libmonero-cpp.dylib
 cd ./external/monero-core/ && 
-make release-static -j8 &&
+make release-static -j$HOST_NCORES &&
 
 # Make libmonero-cpp.dylib
 cd ../../ &&
 mkdir -p build && 
 cd build && 
 cmake .. && 
-cmake --build . && 
+cmake --build . -j$HOST_NCORES && 
 make .
